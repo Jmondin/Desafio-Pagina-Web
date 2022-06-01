95b00bd1c12a5b92bd7d3ef417bd02e8f087cdd0
Jmondin <julio_mondin@terra.com.br>
2022-05-31 21:04:43 -0300

    arquivos de inicio Web

---
+++ Desafio WEB/Pagina.php
@@ -0,0 +1,79 @@
+<html>
+
+    <head>
+        <title>Andamento curso DIO</title>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+        <style type="text/css">
+            .linha {
+                font-weight: bold;
+                color: green;
+                padding-left: 10px;
+                line-height: 50px;
+            }
+        
+            </style>
+        </head>
+
+        <body>
+
+        <?php
+                print( "Programa do curso DIO");
+            
+        ?>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Accordion - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+    <script>
+    $( function() {
+        $( "#accordion" ).accordion();
+    } );
+    </script>
+</head>
+<body>
+
+<div id="accordion">
+    <h3>Section 1</h3>
+    <div>
+        <p>
+        Primeiro passo passar por Modulo 0, se preparando para a Jornada, dividido em 4 atividades
+        </p>    
+    </div>
+    <h3>Section 2</h3>
+    <div>
+        <p>
+        Segundo passo passar por modulo 1, com tres atividades relativas aos Princípios de 
+        Desenvolvimento de Software
+        </p>    
+    </div>
+    <h3>Section 3</h3>
+    <div>
+        <p>
+        Terceiro passo passar por modulo 2, com 4 atividades relacionadas aos Fundamentos de WEB 
+        com HTML e CSS
+        </p>    
+    </div>
+    <h3>Section 4</h3>
+    <div>
+        <p>
+        Quarto Passo passar por modulo 3, com 9 atividades, Conhecendo a Linguagem de Programação
+        JavaScript
+        </p>    
+    </div>
+</div>
+
+</body>
+</html>
+
+
+
